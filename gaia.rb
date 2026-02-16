7d8f5a763c986c96bd3b962191be7986c95abc89
jinhyeong jang <jinhinjala26@naver.com>
2026-02-17 00:00:45 +0900

    Update SHA256 and modify installation process

--- gaia.rb
+++ gaia.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 
 class Gaia < Formula
-  include Language::Python::Virtualenv
 
   desc "Goal-oriented Autonomous Intelligence for Adaptive GUI Testing"
   homepage "https://github.com/capston2025/capston"
   url "https://github.com/capston2025/capston/archive/refs/heads/main.tar.gz"
   version "0.1.0"
-  sha256 "23df20737d102e5353a77857106d20529dbcd05c0ec767affbd601e15c67953a"
+  sha256 "34df00308211c9772bac7ab7869ed702829d63f0eff932e26166a6a90931e7c9"
 
   license "MIT"
 
   depends_on "python@3.12"
 
+  include Language::Python::Virtualenv
+  
   def install
-    venv = virtualenv_create(libexec, Formula["python@3.12"].opt_bin/"python3.12")
-    venv.pip_install Pathname.pwd
-    venv.pip_install_and_link
+    python = Formula["python@3.12"].opt_bin/"python3.12"
+    system python, "-m", "venv", "--system-site-packages", libexec
+
+    system libexec/"bin/python", "-m", "pip", "install", "--upgrade", "pip"
+    system libexec/"bin/python", "-m", "pip", "install", "--verbose", "--no-cache-dir", Pathname.pwd
+    bin.install_symlink libexec/"bin/gaia"
   end
 
 
